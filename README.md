34af2844cdcc3761ea2f1d2c85140fb01cd6f333
Alan Bevier <awbevier@gmail.com>
2022-01-15 15:05:26 -0500

    update

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # rpg
 
+Current Goal: Fully playable turns
+
+- Enemies will attack back
+- Check for win conditions
+- Target handling (Make sure commands can only attack the correct target)
+
+- How to render dead enemies and players
+- Show an attack animation?
+
 Refactor:
 State -> BattleState
 Command -> Attack (Command would trigger an attack i.e. command is the "input" and attack is the "action")
 
-TODO:
+MAJOR TODOS:
 fully playable turns
 win condition check
+
 multiple skills
 stats
 buffs
--- src/scenes/driver.ts
+++ src/scenes/driver.ts
@@ -6,26 +6,14 @@ import { UIState } from './game-scene'
 import { Prompter } from './prompter'
 
 // The main combat loop should be the following steps:
-// - Prompt for actions from each player character
+// - Prompt for commands from each player character
 // - Generate actions for each enemy
+// - Convert all commands into actions and sort by speed
 // - Apply each action to the game state in order
 // - - Render the action result
 // - Restart the loop
 
 const run = async (uiState: UIState, state: State): Promise<void> => {
-  // const value = await Menu.getMenuSelection(scene, [
-  //   { text: '1', value: 1 },
-  //   { text: '2', value: 2 },
-  //   { text: '3', value: 3 },
-  // ])
-  // console.log(`menu clicked with value ${value}`)
-  // const targetId = await Menu.getEnemyClick(uiState)
-  // console.log(`selected target = ${targetId}`)
-  // const command = { speed: 10, sourceId: '1', targetId, type: CommandType.Attack }
-  // const { state: newState } = Command.executeCommand(state, command)
-  // this.renderBattle(newState, uiState)
-
-  // TODO: need to render the battle!!
   const result = await Prompter.promptForCommands(uiState, state)
   console.log('result of NEWEST prompting is:', result)
 
@@ -36,21 +24,19 @@ const run = async (uiState: UIState, state: State): Promise<void> => {
   applyCommands(sorted, uiState, state)
 }
 
-//TODO: can get rid of prompter and renderer when UIState has a scene
-// - Can just statically call Prompter.promtpt(uiState) and Renderer.render(...) instead of needing to bind
 const applyCommands = async (commands: Command[], uiState: UIState, state: State) => {
   if (array.isEmpty(commands)) {
     return run(uiState, state)
   }
 
-  // TODO: This code is a fucking mess...
+  // TODO: This code is a fucking mess... booby traps with newStates
   const [command, ...rest] = commands
-  const result = Command.executeCommand(state, command)
+  const { state: newState, text } = Command.executeCommand(state, command)
 
-  await Prompter.flashPrompt(uiState, result.text, 1000)
+  await Prompter.flashPrompt(uiState, text, 1000)
 
-  const newUIState = BattleRenderer.renderBattle(uiState, result.state)
-  applyCommands(rest, newUIState, result.state)
+  const newUIState = BattleRenderer.renderBattle(uiState, newState)
+  applyCommands(rest, newUIState, newState)
 }
 
 export const Driver = {
